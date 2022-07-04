aa35e19353647b9a6a28db5e8d0c147bcf07575f
Valentin Mega <MegaValentin@users.noreply.github.com>
2022-07-04 11:08:34 -0300

    mage class and warrior class
    The mage and warrior object that inherit from the champion class was added

---
+++ guerrero.py
@@ -0,0 +1,30 @@
+import re
+from zmq import IPC_FILTER_GID
+from campeon import Campeon
+
+class Guerrero(Campeon):
+
+    def __init__(self, nombre, fuerza, defensa, vida, espada):
+        super().__init__(nombre, fuerza, defensa, vida)
+        self.espada = espada
+
+    def cambiar_objeto(self):
+        opcion = int(input("Elige un arma: (1) Espada larga, daño 10. (2) Espada corta, daño 8 "))
+        if opcion == 1:
+            self.espada = 10
+        elif opcion == 2:
+            self.espada = 8
+        else:
+            print("El numero de arma es icorrecto")
+
+    def atributos(self):
+        super().atributos()
+        print("Espada: ", self.espada)
+
+    def golpe_objeto(self, enemigo):
+        return self.fuerza*self.espada - enemigo.defensa
+
+
+gust = Guerrero("facha", 30, 50, 100, 5)
+
+gust.atributos()
---
+++ mago.py
@@ -0,0 +1,23 @@
+from campeon import Campeon
+
+class Mago(Campeon):
+
+    def __init__(self, nombre, fuerza, defensa, vida, libro):
+        super().__init__(nombre, fuerza, defensa, vida)
+        self.libro = libro
+
+    def atributos(self):
+        super().atributos()
+        print("Libro: ", self.libro)
+
+    def cambiar_objeto(self):
+        opcion = int(input("Elige un libro: (1) Libro oscuro, daño 10. (2) Libro de los mares, daño 8 "))
+        if opcion == 1:
+            self.libro = 10
+        elif opcion == 2:
+            self.libro = 8
+        else:
+            print("El numero de librp es icorrecto")
+
+    def golpe_objeto(self, enemigo):
+        return self.fuerza*self.libro - enemigo.defensa
